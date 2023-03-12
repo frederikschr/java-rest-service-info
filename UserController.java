37963450b6f7cd9808e5483bbf8de53265fc926d
Frederik Schier <fredi.schier@gmail.com>
2023-03-12 21:18:46 +0100

    update

--- UserController.java
+++ UserController.java
@@ -7,27 +7,18 @@
  */
 public class UserController
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse UserController
-     */
-    public UserController()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
+    private DatabaseController dbController;
+    
+    public UserController(DatabaseController dbController) {
+        this.dbController = dbController;
     }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+    
+    
+    public void createUser(String userData) {
+        
+    }
+    
+    public void getUser(String userData) {
+    
     }
 }
--- UserHandler.java
+++ UserHandler.java
@@ -8,17 +8,47 @@ import java.io.IOException;
 
 public class UserHandler implements HttpHandler
 {
+    private UserController userController;
+    
+    public UserHandler(DatabaseController dbController) {
+        this.userController = new UserController(dbController);    
+    }
+    
     @Override
-    public void handle(HttpExchange request) throws IOException {
-         if ("GET".equals(request.getRequestMethod())) {
-                String responseText = "Hello World!";
-                request.sendResponseHeaders(200, responseText.getBytes().length);
-                OutputStream output = request.getResponseBody();
-                output.write(responseText.getBytes());
-                output.flush();
-            } else {
-                request.sendResponseHeaders(405, -1);// 405 Method Not Allowed
-            }
-        request.close();
+    public void handle(HttpExchange exchange) throws IOException {
+        
+        
+        String requestBody = new String(exchange.getRequestBody().readAllBytes());
+        
+        OutputStream output = exchange.getResponseBody();
+        
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        
+        if (exchange.getRequestMethod().equals("GET")) {
+            
+            this.userController.getUser(requestBody);            
+        
+            // exchange.sendResponseHeaders(200, responseText.getBytes().length);
+
+            // output.write(responseText.getBytes());
+            output.flush();    
+        }
+        
+        else if (exchange.getRequestMethod().equals("POST")) {
+              
+            
+            
+            
+
+            
+            
+        }
+        
+        else {
+            exchange.sendResponseHeaders(405, -1);
+        }
+        
+        exchange.close();
+        
     }
 }
